9f7d6a45be4a7f83a278a759009f15cf48822fd8
Jakub Miarka <jakvbmiarka@gmail.com>
2024-08-22 18:02:35 +0200

    started extending character class, added items' classes

--- lib/materials/character.dart
+++ lib/materials/character.dart
@@ -1,7 +1,11 @@
 class Character
 {
 String name;
+int exp = 0;
+CharClass charClass = CharClass.none;
+
 
 Character({required this.name});
 }
 
+enum CharClass {none, barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, wizard}
