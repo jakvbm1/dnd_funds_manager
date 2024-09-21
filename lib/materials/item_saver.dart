1570f59225feadaf80db9fea79264ba141902b35
Jakub Miarka <jakvbmiarka@gmail.com>
2024-09-21 15:07:00 +0200

    Methods for getting items from strings added

--- lib/materials/item_saver.dart
+++ lib/materials/item_saver.dart
@@ -1,5 +1,6 @@
 import 'package:dnd_funds_manager/materials/armor.dart';
 import 'package:dnd_funds_manager/materials/character.dart';
+import 'package:dnd_funds_manager/materials/enums.dart';
 import 'package:dnd_funds_manager/materials/regular_item.dart';
 import 'package:dnd_funds_manager/materials/weapon.dart';
 
@@ -34,5 +35,32 @@ class ItemSaver
   {
     String toRet ='r, ${regularItem.name}, ${regularItem.description}';
     return toRet;
+  }
+
+  static Weapon stringToWeapon(String string)
+  {
+    List<String> atts = string.split(', ');
+    List<WeaponType> weaponAtt = [];
+    if(atts.length > 7)
+    {
+      for(int i = 7; i < atts.length; i++)
+      {
+        weaponAtt.add(WeaponType.values.byName(atts[i]));
+      }
+    }
+
+    return Weapon(dice: DiceType.values.byName(atts[6]), name: atts[1], description: atts[2], bonus: int.parse(atts[3]), dmg: DamageType.values.byName(atts[4]), nDices: int.parse(atts[5]), attributes: weaponAtt);
+  }
+
+  static Armor stringToArmor(String string)
+  {
+    List<String> atts = string.split(', ');
+    return Armor(name: atts[1], description: atts[2], armorType: ArmorType.values.byName(atts[3]), bonusStat: Stat.values.byName(atts[4]), maxBonus: int.parse(atts[5]));
+  }
+
+  static RegularItem stringToRegularItem(String string)
+  {
+    List<String> atts = string.split(', ');
+    return RegularItem(name: atts[1], description: atts[2]);
   }
 }
